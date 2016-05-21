de54f350d0ca7b217154d07df2ca5c28cb2bd286
Tomáš Kalvoda <tomas.kalvoda@fit.cvut.cz>
2016-05-21 22:32:04 +0200

    test first few decimal digits

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
+##
+#
+# PiBBP tests
+#
+
 using PiBBP
 using Base.Test
 
-# write your own tests here
-@test digpi(0) == 2
+#
+# Particular digits
+#
+
+@test digpi(0)       == 2
 @test digpi(1000000) == 6
+
+#
+# It actually computes fractional part of $\pi$.
+#
+
+ds = pmap(digpi, 0:14)
+z  = sum(j -> ds[j] / 16^j, 1:length(ds))
+
+@test_approx_eq Float64(pi - 3.) z
