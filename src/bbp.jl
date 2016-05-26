67cdfd8c956cb5a69926a29531c68790e55756c8
Tomáš Kalvoda <tomas.kalvoda@fit.cvut.cz>
2016-05-26 10:51:30 +0200

    improve docstrings with LaTeX

--- src/bbp.jl
+++ src/bbp.jl
@@ -1,18 +1,17 @@
 """
-For integer ``d``, ``k``, and ``j`` compute
-``16^{d-k} \\bmod 8k + j ) / (8k + j)``.
+For integer \$d\$, \$k\$, and \$j\$ compute
+\$\$ 16^{d-k} \\bmod 8k + j ) / (8k + j). \$\$
 """
 ratio(d, k, j) = powermod(16, d - k, 8k + j) / (8k + j)
 
 """
-Fractional part of a real number ``x``.
+Fractional part of a real number \$x\$.
 """
 frac_part(x) = x - floor(x)
 
 """
-For integer ``d`` and ``j`` computes the fractional part of
-`` \\sum_{k=0}^d \\frac{16^{d-k} \\bmod 8k+j}{8k+j} +
-   \\sum_{k=d+1}^\\infty \\frac{1}{16^{k-d} (8k+j)} ``.
+For integer \$d\$ and \$j\$ computes the fractional part of
+\$\$ \\sum_{k=0}^d \\frac{16^{d-k} \\bmod 8k+j}{8k+j} + \\sum_{k=d+1}^\\infty \\frac{1}{16^{k-d} (8k+j)}. \$\$
 """
 function sigma(d, j)
   r = 0.
@@ -35,7 +34,7 @@ function sigma(d, j)
 end
 
 """
-Compute ``d+1``th hexadecimal digit of ``\\pi``. 
+Compute \$d+1\$th hexadecimal digit of \$\\pi\$. 
 """
 function digpi(d)
   r = 4*sigma(d, 1) - 2*sigma(d, 4) - sigma(d, 5) - sigma(d, 6)
