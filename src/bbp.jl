3cb45887eab591a7f5703ff3817cbc6a2773f511
Tomáš Kalvoda <tomas.kalvoda@fit.cvut.cz>
2016-05-28 09:30:56 +0200

    minor improvements

--- src/bbp.jl
+++ src/bbp.jl
@@ -1,8 +1,25 @@
+
+"""
+For integers ``a``, ``b``, and ``n`` efficiently compute
+``a^b \\bmod n``.
+"""
+@fastmath function power_mod(a, b, n)
+  r = 1
+  while b > 0
+    if isodd(b)
+      r = r * a % n
+    end
+    b = div(b, 2)
+    a = a * a % n
+  end
+  return r
+end
+
 """
 For integer \$d\$, \$k\$, and \$j\$ compute
 \$\$ ( 16^{d-k} \\bmod 8k + j ) / (8k + j). \$\$
 """
-ratio(d, k, j) = powermod(16, d - k, 8k + j) / (8k + j)
+ratio(d, k, j) = power_mod(16, d - k, 8k + j) / (8k + j)
 
 """
 Fractional part of a real number \$x\$.
@@ -15,19 +32,22 @@ For integer \$d\$ and \$j\$ computes the fractional part of
 """
 function sigma(d, j)
   r = 0.
-  for k in 0:d
+  @fastmath @simd for k in 0:d
     r += ratio(d, k, j)
     if r > 1.0
       r = frac_part(r)
     end
   end
 
-  k = d + 1
-  u = 1 / (16^(k - d) * (8k + j)) 
-  while u > eps(Float64)
-    r += u
-    k += 1
-    u = 1 / ((16^(k-d))*(8k + j))
+  kk  = 8. * (d + 1) 
+  u   = 1 / 16
+  rat = u / kk
+
+  @fastmath while rat > eps(Float64)
+    r   += rat
+    kk  += 8.
+    u   /= 16.
+    rat  = u / kk
   end
 
   return frac_part(r)
